15e88dfbb3e71f3482354cd7a10789595a2f025c
mandalPappu <mandalpappu62021@gmail.com>
2024-08-23 21:07:55 +0530

    error handle

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,24 +1,9 @@
 'use client'
+import React, { ReactNode } from "react"
 import Admin from "@/components/Admin";
-import axios from "axios";
-import { headers } from "next/headers";
-import { ChangeEvent, FormEvent, HTMLInputTypeAttribute, MouseEvent, useState } from "react";
 
-const page = () => {
-  const [name, setName] = useState("")
-  const [Image, setImage] = useState<FileList | null>(null)
-  
+const page:React.FC = ():ReactNode => {
 
-  const handleSubmit = (e:MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    if(!Image) return null
-    const formData = new FormData();
-    formData.append("name", name);
-    for (let i = 0; i < Image.length; i++) {
-      formData.append("Image", Image[i]); 
-    }
-    axios.post("/api/carousel", formData)
-  }
   return (
     <div className="w-full h-full">
       <Admin />
