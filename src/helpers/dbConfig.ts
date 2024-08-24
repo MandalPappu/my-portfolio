e897f63053b1f34dbfb7a7a3f4db7834f18fd2a4
mandalPappu <mandalpappu62021@gmail.com>
2024-08-24 21:24:29 +0530

    change some code

--- src/helpers/dbConfig.ts
+++ src/helpers/dbConfig.ts
@@ -19,7 +19,7 @@ async function dbConnect(): Promise<void> {
     try {
         // Attempt to connect to the database
         
-        const db = await mongoose.connect(`${process.env.MONGODB_URI!}/${DB_NAME}` || '', {});
+        const db = await mongoose.connect(`${process.env.MONGODB_URI!}/${DB_NAME}` || '');
 
         connection.isConnected = db.connections[0].readyState;
 
